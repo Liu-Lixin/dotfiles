d26ad98ddf96639c2bfe08e013853653fa9d79dc
Lixin Liu <liulixin4work@icloud.com>
2024-09-21 21:18:40 +0800

    MOF: bootstrap to link

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
-
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	find . -type f \
+		! -path "./.git/*" \
+		! -name ".DS_Store" \
+		! -name "brew.sh" \
+		! -name "bootstrap.sh" \
+		! -name "README.md" \
+		-print0 | while IFS= read -r -d '' file; do
+		target="$HOME/${file#./}"
+		if [ -e "$target" ]; then
+			rm "$target"
+		fi
+		ln -s "$(pwd)/$file" "$target"
+	done
+	source ~/.zshrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -p "This may delete existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
